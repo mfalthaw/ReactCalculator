64dc6c239124f48fb3ba286fe213d0c4d723fb20
mfalthaw <mfalthaw@ucalgary.ca>
2018-08-08 02:37:16 +0300

    reorganizing

--- src/ReactCalculator.js
+++ src/ReactCalculator.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ReactCalculator from './src/ReactCalculator';
 
-export default class App extends React.Component {
+export default class ReactCalculator extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ReactCalculator/>
+        <Text>Hello, React!!!!</Text>
       </View>
     );
   }
